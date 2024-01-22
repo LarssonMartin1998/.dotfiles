990227893ecee51f86216e9e1ffc72eaedefbafc
Martin Larsson <larssonmartin1998@gmail.com>
2024-01-22 01:03:04 +0100

    Make sure we never add whitespaces using surround

--- nvim/lua/plugs/surround.lua
+++ nvim/lua/plugs/surround.lua
@@ -3,6 +3,14 @@ return {
     version = "*",
     event = "VeryLazy",
     config = function()
-        require("nvim-surround").setup({})
+        require("nvim-surround").setup({
+            -- Make sure that we never get whitespaces when adding surroundings
+            surrounds = {
+                ["("] = { add = { "(", ")" }, },
+                ["{"] = { add = { "{", "}" }, },
+                ["<"] = { add = { "<", ">" }, },
+                ["["] = { add = { "[", "]" }, },
+            }
+        })
     end,
 }
