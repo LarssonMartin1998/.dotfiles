b21ebf8043164a3287fa33b0cd58b573d01008f6
Martin Larsson <larssonmartin1998@gmail.com>
2024-04-07 02:10:08 +0200

    Add hyprland config

---
+++ hypr/hyprpaper_preload.sh
@@ -0,0 +1,10 @@
+monitor="DP-1"
+wallpaper_dir="$HOME/dev/git/.dotfiles/wallpapers/catppuccin"
+wallpapers=$(find "$wallpaper_dir" -type f | sort -R)
+
+for wallpaper in $wallpapers; do
+    preload_string+="preload = "$wallpaper"\n"
+    wallpaper_string+="wallpaper = $monitor, $wallpaper\n"
+done
+
+echo -en "$preload_string$wallpaper_string\nsplash=false" > ~/.config/hypr/hyprpaper.conf
