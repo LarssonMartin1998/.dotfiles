f6dee75d2c0f9ebd945fa9f7425c86c4940499d1
Martin Larsson <larssonmartin1998@gmail.com>
2024-04-08 23:17:01 +0200

    Add laststatus = 3 so that we can see fillchars for splits, additionally, ran formatter

--- nvim/lua/vim_opt.lua
+++ nvim/lua/vim_opt.lua
@@ -1,16 +1,16 @@
 local opt = vim.opt
 
- -- Disable tabs, will use telescope and harpoon instead
+-- Disable tabs, will use telescope and harpoon instead
 opt.showtabline = 0
 
- -- Make Vim use the system clipboard
+-- Make Vim use the system clipboard
 opt.clipboard = "unnamedplus"
 
- -- Highlight the currently selected row
+-- Highlight the currently selected row
 opt.cursorline = true
 opt.cursorlineopt = "both"
 
- -- Indenting
+-- Indenting
 opt.expandtab = true
 opt.smartindent = true
 opt.breakindent = true
@@ -18,41 +18,43 @@ opt.shiftwidth = 4
 opt.tabstop = 4
 opt.softtabstop = 4
 
- -- Disable home screen
+-- Disable home screen
 opt.shortmess:append("sI")
 
- -- Signcolumn
-opt.signcolumn = "yes:1" -- Adds a spacing to the left which can contain gutter icons
+-- Signcolumn
+opt.signcolumn = "yes:1"      -- Adds a spacing to the left which can contain gutter icons
 opt.fillchars = { eob = " " } -- Remove the fill character for empty lines which defaults to: "~"
 
- -- Search
+-- Search
 opt.ignorecase = true
 opt.smartcase = true
 opt.incsearch = true
 
- -- Disable mouse support
+-- Disable mouse support
 opt.mouse = ""
 
- -- Numbers
+-- Numbers
 opt.number = true
 opt.relativenumber = true
 opt.numberwidth = 4
 
- -- Decrease update time
+-- Decrease update time
 opt.updatetime = 250
 opt.timeoutlen = 300
 
- -- Richer colors in terminal, not all terminals support this
+-- Richer colors in terminal, not all terminals support this
 opt.termguicolors = true
 
- -- Disable swapfile, 99/100 times it just gets in the way
+-- Disable swapfile, 99/100 times it just gets in the way
 opt.swapfile = false
 
- -- Buffers
+-- Buffers
 opt.splitright = true
 opt.splitbelow = true
 
- -- Removes the extra command line bar at the bottom, using lualine instead
+-- Removes the extra command line bar at the bottom, using lualine instead
 opt.cmdheight = 0
+
+opt.laststatus = 3
 
 return opt
