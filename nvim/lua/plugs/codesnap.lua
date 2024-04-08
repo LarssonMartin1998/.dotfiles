05c437bfcc822abc40fc9d5c56588f0948509606
Martin Larsson <larssonmartin1998@gmail.com>
2024-04-08 10:48:39 +0200

    Add codesnap

---
+++ nvim/lua/plugs/codesnap.lua
@@ -0,0 +1,23 @@
+return {
+    "mistricky/codesnap.nvim",
+    build = "make",
+    config = function()
+        require("codesnap").setup({
+            mac_window_bar = true,
+            title = "codesnap.nvim",
+            code_font_family = "JetBrainsMono Nerd Font",
+            breadcrumbs_separator = "/",
+            has_breadcrumbs = true,
+            bg_theme = "grape",
+            watermark = "",
+        })
+
+        require("utils").add_keymaps({
+            v = {
+                ["<leader>cs"] = {
+                    cmd = ":CodeSnap<CR>"
+                },
+            }
+        })
+    end,
+}
