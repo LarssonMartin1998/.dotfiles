c507d0f7b66567591a1e14451ea4586e958c69de
Martin Larsson <larssonmartin1998@gmail.com>
2024-05-25 14:45:00 +0200

    Small color tweaks in lualine

--- home/.config/nvim/lua/plugs/lualine.lua
+++ home/.config/nvim/lua/plugs/lualine.lua
@@ -4,9 +4,16 @@ return {
         "nvim-tree/nvim-web-devicons"
     },
     config = function()
+        local catppuccin_theme = require("lualine.themes.catppuccin")
+        local slightly_darker_surface0 = "#2c3045"
+        catppuccin_theme.normal.c.bg = slightly_darker_surface0
+        -- TODO: Change the colors of the lualine theme to match the statusbar from the catppuccin theme in zellij
+        -- The lualine is already using the catppuccin theme, but the design is not consistent with the zellij statusbar ...
+        -- Very nitpicky, but it would be nice to have a consistent design
+
         require("lualine").setup {
             options = {
-                theme = "catppuccin",
+                theme = catppuccin_theme,
                 section_separators = { "", "" },
                 component_separators = { "", "" },
                 icons_enabled = true,
@@ -14,7 +21,7 @@ return {
             sections = {
                 lualine_a = { "mode" },
                 lualine_b = { "branch" },
-                lualine_c = { require("auto-session.lib").current_session_name },
+                lualine_c = {},
                 lualine_x = { "encoding", "fileformat", "filetype" },
                 lualine_y = { "progress" },
                 lualine_z = { "location" }
