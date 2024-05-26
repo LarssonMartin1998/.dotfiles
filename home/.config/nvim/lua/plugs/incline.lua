761f6e3e59a212948774c646c7c532716790df0e
Martin Larsson <larssonmartin1998@gmail.com>
2024-05-26 18:10:53 +0200

    Fix overlapping in incline statusbars diagnostic section

--- home/.config/nvim/lua/plugs/incline.lua
+++ home/.config/nvim/lua/plugs/incline.lua
@@ -46,7 +46,7 @@ return {
                         local n = #vim.diagnostic.get(props.buf,
                             { severity = vim.diagnostic.severity[string.upper(severity)] })
                         if n > 0 then
-                            table.insert(label, { icon .. n .. " ", group = "DiagnosticSign" .. severity })
+                            table.insert(label, { icon .. " " .. n .. " ", group = "DiagnosticSign" .. severity })
                         end
                     end
                     if #label > 0 then
