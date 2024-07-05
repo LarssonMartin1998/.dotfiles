7c459121947797b96f0061dcba48d0a065ba1baf
Martin Larsson <larssonmartin1998@gmail.com>
2024-07-05 16:44:23 +0200

    Make sure that we don't try and load obsidian when we aren't connected to the local network with the obsidian server

--- home/.config/nvim/lua/plugs/obsidian.lua
+++ home/.config/nvim/lua/plugs/obsidian.lua
@@ -10,6 +10,12 @@ return {
         local scndbrn_path = vim.fn.has("unixmac") == 1 and
             "~/rp4-data/obsidian-vault/scndbrn" or
             "/mnt/rp4-data/obsidian-vault/scndbrn"
+
+        local is_vault_accessible = scndbrn_path == nil or vim.fn.isdirectory(scndbrn_path) == 0
+        if is_vault_accessible then
+            return
+        end
+
         require("obsidian").setup({
             workspaces = {
                 {
