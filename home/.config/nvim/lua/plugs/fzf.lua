fc01aa096c27f0d4fca6b01fd5ff7309ace7d05c
Martin Larsson <martin.larsson@king.com>
2024-09-16 00:12:30 +0200

    Completely change from telescope to fzf, it's way more performant

--- home/.config/nvim/lua/plugs/fzf.lua
+++ home/.config/nvim/lua/plugs/fzf.lua
@@ -1,19 +1,47 @@
 return {
     "ibhagwan/fzf-lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    opts = {},
+    opts = {
+        "telescope",
+        winopts = {
+            preview = {
+                default = "bat"
+            }
+        }
+    },
     config = function()
         local fzf = require("fzf-lua")
-        require("utils").add_keymaps({
-            n = {
-                ["<leader>to"] = {
-                    cmd = function()
-                        fzf.files({
-                            git_icons = false,
-                        })
-                    end
-                }
+        local pickers = {
+            {
+                mapping = "o",
+                action = function()
+                    fzf.files({
+                        git_icons = false,
+                    })
+                end
+            },
+            {
+                mapping = "a",
+                action = function()
+                    fzf.live_grep_native({
+                        git_icons = false,
+                        rg_opts = "--hidden --column --line-number --no-heading --color=always --smart-case",
+                        fzf_opts = {
+                            ['--exact'] = false, -- Disable exact matching
+                        },
+                    })
+                end
             }
-        })
+        }
+
+        local keymaps = {}
+        keymaps.n = {}
+        for _, picker in ipairs(pickers) do
+            keymaps.n["<leader>t" .. picker.mapping] = {
+                cmd = picker.action
+            }
+        end
+
+        require("utils").add_keymaps(keymaps)
     end
 }
