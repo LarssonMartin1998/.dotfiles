f909d576fd323950ef67bc8a8a16f1e5c4621f50
Martin Larsson <martin.larsson@king.com>
2024-10-08 01:21:56 +0200

    Add modules to Neorg: defaults, dirman, concealer

--- home/.config/nvim/lua/plugs/neorg.lua
+++ home/.config/nvim/lua/plugs/neorg.lua
@@ -1,6 +1,18 @@
 return {
     "nvim-neorg/neorg",
-    opts = {},
+    opts = {
+        load = {
+            ["core.defaults"] = {},
+            ["core.dirman"] = {
+                config = {
+                    workspaces = {
+                        my_workspace = "~/.neorg",
+                    },
+                },
+            },
+            ["core.concealer"] = {},
+        },
+    },
     event = "VeryLazy",
     lazy = true,
 }
