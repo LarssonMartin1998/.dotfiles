6c605b47c2a3eca4552735b3a7f8e04b47227284
Martin Larsson <martin.larsson@king.com>
2024-10-24 19:09:31 +0200

    Add tiny inline diagnostics and fix $TERM in tmux and alacritty to alacritty instead of xterm-256color

--- home/.config/nvim/lua/plugs/tiny-inline-diagnostics.lua
+++ home/.config/nvim/lua/plugs/tiny-inline-diagnostics.lua
@@ -7,5 +7,13 @@ return {
         vim.diagnostic.config({
             virtual_text = false,
         })
+
+    vim.cmd [[
+        highlight DiagnosticUnderlineError gui=undercurl guisp=#ff0000   " Red for error
+        highlight DiagnosticUnderlineWarn gui=undercurl guisp=#ffaa00    " Yellow/Orange for warning
+        highlight DiagnosticUnderlineHint gui=undercurl guisp=#00ffff    " Cyan for hint
+        highlight DiagnosticUnderlineInfo gui=undercurl guisp=#0000ff    " Blue for info
+    ]]
+
     end
 }
