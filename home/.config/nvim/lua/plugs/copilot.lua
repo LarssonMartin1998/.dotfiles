9b9424ac05834bc83eb8373e1e0e48e7eb2c1dfc
Martin Larsson <martin.larsson@king.com>
2024-10-30 17:45:36 +0100

    Change keybindings for copilot to better match cmd workflow (completion on tab and acceping auto complete on right arrow)

--- home/.config/nvim/lua/plugs/copilot.lua
+++ home/.config/nvim/lua/plugs/copilot.lua
@@ -1,3 +1,18 @@
 return {
     "github/copilot.vim",
+    config = function()
+        vim.g.copilot_no_tab_map = true
+        require("utils").add_keymaps({
+            i = {
+                ["<Right>"] = {
+                    cmd = 'copilot#Accept("")',
+                    opts = {
+                        expr = true,
+                        replace_keycodes = false,
+                        silent = true,
+                    }
+                }
+            }
+        })
+    end
 }
