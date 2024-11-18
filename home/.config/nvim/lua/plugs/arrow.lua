edeeb04e6f25d0e1ec4ea7d0a96e388fd81a40c4
Martin Larsson <martin.larsson@king.com>
2024-11-19 00:52:40 +0100

    separate serialized arrow data by git branch.

--- home/.config/nvim/lua/plugs/arrow.lua
+++ home/.config/nvim/lua/plugs/arrow.lua
@@ -6,6 +6,7 @@ return {
         show_icons = true,
         leader_key = ",",
         global_bookmarks = false,
+        separate_by_branch = true,
         custom_actions = {
             open = function(filename, _)
                 vim.cmd(string.format(":drop %s", filename))
