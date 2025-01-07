f0f10ab09b7bf8514c3653b21115457d6c76dac9
Martin Larsson <larssonmartin1998@gmail.com>
2025-01-07 23:49:17 +0100

    Add language toggle between SE and US in Sway, additionally, set Ghostty to the default terminal.

---
+++ linux_home/.config/sway/toggle_keyboard_layout.sh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# Toggle between US (0) and Swedish (1) layouts
+current=$(swaymsg -t get_inputs | jq -r '.[] | select(.type=="keyboard") | .xkb_active_layout_index' | head -n 1)
+
+if [ "$current" -eq 0 ]; then
+    swaymsg input '*' xkb_switch_layout 1
+else
+    swaymsg input '*' xkb_switch_layout 0
+fi
