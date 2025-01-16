d4e1e4b891ecb1afca7840fca6b14516350f1397
Martin Larsson <larssonmartin1998@gmail.com>
2025-01-16 23:09:16 +0000

    Add lots of programs to HM

--- nix/home.nix
+++ nix/home.nix
@@ -3,6 +3,19 @@
 	    stateVersion = "24.05";
         packages = with pkgs; [
             neovim
+            fzf
+            bat
+            git
+            yazi
+            zsh
+            tmux
+            eza
+            curl
+            wget
+            ripgrep
+            fd
+            jq
+            starship
         ];
     };
 }
