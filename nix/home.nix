fb2adb45471816d8b96385f4af312ab776e8f640
Martin Larsson <larssonmartin1998@gmail.com>
2025-01-16 23:49:38 +0000

    Move files out of their respective fs structure into program dirs in root and symlink dotfils using HM.

--- nix/home.nix
+++ nix/home.nix
@@ -20,9 +20,18 @@
             fd
             jq
             starship
+            fastfetch
         ];
         file = {
             ".zshrc".source = ../zsh/.zshrc;
+            ".config/nvim".source = ../nvim;
+            ".config/yazi".source = ../yazi;
+            ".config/starship.toml".source = ../starship/starship.toml;
+            ".tmux.conf".source = ../tmux/.tmux.conf;
+            ".config/tmux".source = ../tmux;
+            ".config/bat".source = ../bat;
+            ".config/ghostty".source = ../ghostty;
+            ".config/confutils".source = ../confutils;
         };
     };
 }
