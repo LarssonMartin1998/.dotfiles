7be365b419cf5d866f884fc1ca7a4fe7f405075d
Martin Lasson <larssonmartin1998-mac@Martins-MacBook-Pro.local>
2025-01-26 00:30:51 +0100

    Add local home values

--- nix/local_home.nix
+++ nix/local_home.nix
@@ -6,7 +6,7 @@
   # accordingly, and then commit it.
   home = {
     # Stub values for demonstration. Override these in local branch.
-    username = "JohnDoe";
-    homeDirectory = "/home/johndoe";
+    username = "larssonmartin1998-mac";
+    homeDirectory = "/Users/larssonmartin1998-mac";
   };
 }
