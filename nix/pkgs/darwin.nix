f5e7488f6ee29b43f07c94c80a2ea87a9538d3d8
Martin Lasson <larssonmartin1998-mac@Martins-MacBook-Pro.local>
2025-01-29 18:43:58 +0100

    Add aerospace to darwin config

--- nix/pkgs/darwin.nix
+++ nix/pkgs/darwin.nix
@@ -1,10 +1,12 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   home = {
     packages = with pkgs; [
       gawk
     ];
     file = {
+      ".config/aerospace/aerospace.toml".source =
+        config.lib.file.mkOutOfStoreSymlink ../../aerospace/aerospace.toml;
     };
   };
 }
--- nix/system/darwin.nix
+++ nix/system/darwin.nix
@@ -16,6 +16,7 @@
     casks = [
       "ghostty"
       "zen-browser"
+      "nikitabobko/tap/aerospace"
     ];
     brews = [
       "mas"
