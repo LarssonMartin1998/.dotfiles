3fa4d50d74fe1eb2f05dc4ff6cd4c2920a339f3a
Martin Lasson <larssonmartin1998-mac@Martins-MacBook-Pro.local>
2025-02-04 23:36:30 +0100

    Update ibl to set a different highlight for current scope

--- nvim/lua/plugs/indent_blankline.lua
+++ nvim/lua/plugs/indent_blankline.lua
@@ -2,10 +2,31 @@ return {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     event = "BufRead",
-    opts = {
-        debounce = 100,
-        scope = {
-            enabled = false
-        },
-    }
+    config = function()
+        local ibl = require("ibl")
+        ibl.setup({
+            debounce = 100,
+            indent = {
+                char = "▏",
+            },
+            scope = {
+                enabled = true,
+                char = "▏",
+                show_start = false,
+                show_end = false,
+                highlight = { "IblScope" },
+            },
+        })
+
+        local colors = require("ayu.colors")
+        colors.generate(true)
+        local highlights = {
+            { "IblScope",          { fg = colors.keyword } },
+            { "@ibl.scope.char.1", { fg = colors.keyword } },
+        }
+
+        for _, hl in ipairs(highlights) do
+            vim.api.nvim_set_hl(0, hl[1], hl[2])
+        end
+    end
 }
