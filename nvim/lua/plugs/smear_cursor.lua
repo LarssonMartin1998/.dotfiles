3d3de5d221f96f4182a69892a4461517968d6b4d
Martin Larsson <larssonmartin1998@gmail.com>
2025-02-17 18:00:18 +0000

    Add smear cursor plugin to neovim

---
+++ nvim/lua/plugs/smear_cursor.lua
@@ -0,0 +1,19 @@
+return {
+    "sphamba/smear-cursor.nvim",
+    opts = {
+        smear_between_buffers = true,
+
+        min_horizontal_distance_smear = 3,
+        min_vertical_distance_smear = 2,
+
+        scroll_buffer_space = true,
+        smear_insert_mode = false,
+
+        cursor_color = "#d3cdc3",
+        transparent_bg_fallback_color = "#303030",
+
+        stiffness = 0.85,
+        trailing_stiffness = 0.7,
+        distance_stop_animating = 0.5,
+    },
+}
