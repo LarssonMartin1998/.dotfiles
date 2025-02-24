deba4a64a65904e8413f5b42d5291b4dc82c3618
Martin Larsson <larssonmartin1998@gmail.com>
2025-02-24 14:55:08 +0000

    Add persistence session manager, remove auto-session, add keys to dashboard in snacks

--- nvim/lua/plugs/snacks.lua
+++ nvim/lua/plugs/snacks.lua
@@ -8,7 +8,20 @@ return {
             what = "repo",
         },
         picker = { enabled = true, },
-        dashboard = { enabled = true, },
+        dashboard = {
+            enabled = true,
+            preset = {
+                keys = {
+                    { icon = " ", key = "f", desc = "Find File", action = ":lua Snacks.dashboard.pick('files')" },
+                    { icon = " ", key = "n", desc = "New File", action = ":ene | startinsert" },
+                    { icon = " ", key = "g", desc = "Find Text", action = ":lua Snacks.dashboard.pick('live_grep')" },
+                    { icon = " ", key = "c", desc = "Config", action = ":lua Snacks.dashboard.pick('files', {cwd = vim.fn.stdpath('config')})" },
+                    { icon = " ", key = "s", desc = "Restore Session", section = "session" },
+                    { icon = "󰒲 ", key = "L", desc = "Lazy", action = ":Lazy", enabled = package.loaded.lazy ~= nil },
+                    { icon = " ", key = "q", desc = "Quit", action = ":qa" },
+                },
+            },
+        },
         debug = { enabled = true, },
         indent = {
             enabled = true,
