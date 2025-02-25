2e6646dbb0b3210fdf6d9cbfce61a08994848736
Martin Larsson <larssonmartin1998@gmail.com>
2025-02-25 14:10:47 +0000

    Add a command for creating launch.json templates

--- nvim/lua/dap/setup.lua
+++ nvim/lua/dap/setup.lua
@@ -1,31 +1,33 @@
 local utils = require("utils")
 local dap = require("dap")
 
---[[
-.vscode/launch.json:
-----------------------------
-{
-    "version": "0.2.0",
-    "configurations": [
-        {
-            "type": "name_of_adapter",
-            "request": "launch/attach",
-            "name": "Friendly name",
-            "program": "${workspaceFolder}/path/to/executable",
-            "cwd": "${workspaceFolder}",
-            "args": [],
-            "stopOnEntry": false,
-            "environment": []
-        }
-    ]
-}
-----------------------------
-]]
-
 local dir_path = "dap/adapters"
 utils.foreach(utils.get_file_names_in_dir(dir_path, "*.lua", true), function(adapter)
     dap.adapters[adapter] = require(dir_path .. "/" .. adapter)
 end)
+
+vim.api.nvim_create_user_command("LaunchTemplate", function()
+    local template = {
+        '{',
+        '    "version": "0.2.0",',
+        '    "configurations": [',
+        '        {',
+        '            "type": "codelldb",',
+        '            "request": "launch",',
+        '            "name": "Launch",',
+        '            "program": "${workspaceFolder}/build/binary",',
+        '            "cwd": "${workspaceFolder}",',
+        '            "args": [],',
+        '            "stopOnEntry": false,',
+        '            "environment": []',
+        '        }',
+        '    ]',
+        '}',
+    }
+
+    local bufnr = vim.api.nvim_get_current_buf()
+    vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, template)
+end, {})
 
 -- Do not define default fallbacks until I have a better way of handling a default selected configuration.
 -- I never want to be prompted for a configuration, we should have ae serialized active config which is always run unless changed.
