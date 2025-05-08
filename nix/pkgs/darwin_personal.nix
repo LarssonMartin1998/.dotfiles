451b49485100ed4567e6667af370493e7ba28d7a
Martin Larsson <larssonmartin1998@gmail.com>
2025-05-08 19:14:17 +0200

    Remove vector code stuff

--- nix/pkgs/darwin_personal.nix
+++ nix/pkgs/darwin_personal.nix
@@ -1,17 +1,9 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   home = {
     packages = with pkgs; [
-      (python313.withPackages (pythonPkgs: [
-        pythonPkgs.pipx
-      ]))
       clang
       clang-tools
     ];
   };
-
-  home.activation.installVectorcode = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    echo "Installing VectorCode with pipx..."
-    $DRY_RUN_CMD ${pkgs.python311Packages.pipx}/bin/pipx install --force vectorcode
-  '';
 }
--- nix/pkgs/darwin_work.nix
+++ nix/pkgs/darwin_work.nix
@@ -1,10 +1,9 @@
-{ pkgs, lib, ... }:
+{ pkgs, ... }:
 {
   home = {
     packages = with pkgs; [
       (python311.withPackages (pythonPkgs: [
         pythonPkgs.pip
-        pythonPkgs.pipx
         pythonPkgs.setuptools
       ]))
       pcre
@@ -12,12 +11,4 @@
       mkdocs
     ];
   };
-
-      (python313.withPackages (pythonPkgs: [
-        pythonPkgs.pipx
-      ]))
-  home.activation.installVectorcode = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    echo "Installing VectorCode with pipx..."
-    $DRY_RUN_CMD ${pkgs.python311Packages.pipx}/bin/pipx install --force vectorcode
-  '';
 }
