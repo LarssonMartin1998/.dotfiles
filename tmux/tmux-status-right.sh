f993785b19e0da79a8f45a393c957294e7f3988c
Martin Larsson <larssonmartin1998@gmail.com>
2025-05-28 12:31:41 +0200

    fix: Fix spacing of tmux status pane

--- tmux/tmux-status-right.sh
+++ tmux/tmux-status-right.sh
@@ -2,7 +2,7 @@ format_for_tmux() {
     fg="#1F2430"
     bg="#73D0FF"
     text_col="#CCCAC2"
-    echo $1 | awk -v bg="$bg" -v fg="$fg" -v text_col="$text_col" '{printf("#[default]#[fg="bg"]#[default]#[bg="bg", fg="fg", bold] %s #[default]#[fg="bg"]#[default]#[fg="text_col"] %s", substr($0, 1, 1), substr($0, 2))}' 
+    echo $1 | gawk -v bg="$bg" -v fg="$fg" -v text_col="$text_col" '{printf("#[default]#[fg="bg"]#[default]#[bg="bg", fg="fg", bold]%s #[default]#[fg="bg"]#[default]#[fg="text_col"] %s", substr($0, 1, 1), substr($0, 2))}' 
 }
 
 battery_result=$($HOME/.config/confutils/get-battery.sh)
