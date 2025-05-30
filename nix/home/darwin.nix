50b49e5478910e65050ce5afabc242541464d878
Martin Larsson <martin.larsson@king.com>
2025-05-30 16:23:11 +0200

    Small refactor in sys darwin, bw cli is part of default home config now,       use util lib for dotfile linking

--- nix/home/darwin.nix
+++ nix/home/darwin.nix
@@ -1,22 +1,23 @@
-{ pkgs, config, ... }:
+{
+  pkgs,
+  config,
+  ...
+}:
 let
-  bitwarden_cli = pkgs.bitwarden-cli.overrideAttrs (oldAttrs: {
-    stdenv = pkgs.llvmPackages_18.stdenv;
-    nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [ pkgs.llvmPackages_18.stdenv.cc ];
-    CXX = "${pkgs.llvmPackages_18.clang}/bin/clang++";
-    CC = "${pkgs.llvmPackages_18.clang}/bin/clang";
-  });
+  utils = import ../utils.nix;
+  dotfiles = [
+    [
+      ".config/aerospace"
+      "aerospace"
+    ]
+  ];
 in
 {
   home = {
     packages = with pkgs; [
       gawk
       discord
-      bitwarden_cli
     ];
-    file = {
-      ".config/aerospace/aerospace.toml".source =
-        config.lib.file.mkOutOfStoreSymlink ../../aerospace/aerospace.toml;
-    };
+    file = utils.mk_symlinks { inherit config dotfiles; };
   };
 }
