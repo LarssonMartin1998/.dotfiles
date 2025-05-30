d6c8018a2d5ec9bcf3abe4e8a682a8e81957a42a
Martin Larsson <larssonmartin1998@gmail.com>
2025-05-30 15:29:29 +0200

    Enable diagnostic real-time updates from rust LSP

--- nvim/lua/plugs/rustaceanvim.lua
+++ nvim/lua/plugs/rustaceanvim.lua
@@ -10,10 +10,26 @@ return {
             tools = {
                 hover_actions = {
                     auto_focus = true,
+                    replace_builtin_hover = true,
                 },
             },
             server = {
                 on_attach = require("lsplib").configure_generic_client,
+                default_settings = {
+                    ["rust-analyzer"] = {
+                        inlayHints = {
+                            chainingHints = true,
+                            parameterHints = true,
+                            typeHints = true,
+                        },
+                        diagnostics = {
+                            enable = true,
+                            experimental = {
+                                enable = true,
+                            },
+                        },
+                    },
+                },
             }
         }
     end,
