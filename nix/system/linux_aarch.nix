f6af733634a64f40923e64717081915e9630633b
Martin Larsson <larssonmartin1998@gmail.com>
2025-06-15 14:43:48 +0200

    Add kernel settings and tweak TLP to improve battery life

--- nix/system/linux_aarch.nix
+++ nix/system/linux_aarch.nix
@@ -1,11 +1,15 @@
-{ apple-silicon-support, pkgs, ... }:
+{ apple-silicon-support, ... }:
 {
   imports = [
     apple-silicon-support.nixosModules.apple-silicon-support
   ];
 
   boot = {
-    kernelParams = [ "apple_dcp.show_notch=0" ];
+    kernelParams = [
+      "apple_dcp.show_notch=0"
+      "cpuidle.off=0"
+      "mem_sleep_default=deep"
+    ];
     loader.efi.canTouchEfiVariables = false;
   };
 
@@ -19,6 +23,14 @@
   networking.hostName = "asahi-nixos";
 
   services = {
-    tlp.enable = true;
+    tlp = {
+      enable = true;
+      settings = {
+        SOUND_POWER_SAVE_ON_BAT = 1;
+        RUNTIME_PM_ON_BAT = "auto";
+        WIFI_PWR_ON_BAT = "on";
+        WOL_DISABLE = "Y";
+      };
+    };
   };
 }
