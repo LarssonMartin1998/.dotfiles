564c39dcdba645debfc210d5aafc9128f923dc7c
Martin Larsson <larssonmartin@M3-MBP-Max.local>
2025-08-30 16:18:56 +0200

    Remove ff keybinding from fff

--- nvim/lua/plugs/fff.lua
+++ nvim/lua/plugs/fff.lua
@@ -4,11 +4,4 @@ return {
     -- No need to lazy-load with lazy.nvim.
     -- This plugin initializes itself lazily.
     lazy = false,
-    keys = {
-        {
-            "ff", -- try it if you didn't it is a banger keybinding for a picker
-            function() require('fff').find_files() end,
-            desc = 'FFFind files',
-        }
-    }
 }
