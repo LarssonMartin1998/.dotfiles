ef7e706f19528840b2be1f942aea9240fb91ec17
Martin Larsson <larssonmartin1998@gmail.com>
2026-03-19 00:30:58 +0100

    Add demicolon nvim plugin

---
+++ nvim/lua/plugs/demicolon.lua
@@ -0,0 +1,8 @@
+return {
+    "mawkler/demicolon.nvim",
+    dependenies = {
+        "nvim-treesitter/nvim-treesitter",
+        "nvim-treesitter/nvim-treesitter-textobjects",
+    },
+    opts = {},
+}
