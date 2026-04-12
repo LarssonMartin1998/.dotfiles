8b38a13192b9786a1bf8e4ea4786bc81111c0b5a
Martin Larsson <larssonmartin1998@gmail.com>
2026-04-12 02:30:51 +0200

    replace live-rename with rephrase

--- nvim/plugin/rephrase.lua
+++ nvim/plugin/rephrase.lua
@@ -1,4 +1,8 @@
-vim.opt.runtimepath:append("/Users/larssonmartin/dev/git/nvim-rephrase")
-if pcall(require, "rephrase") then
-    require("rephrase").setup({})
-end
+vim.api.nvim_create_autocmd("LspAttach", {
+    once = true,
+    callback = function()
+        vim.pack.add({ "https://github.com/LarssonMartin1998/nvim-rephrase" })
+        require("rephrase").setup({})
+        require("utils").set_keymap_list({ { "<leader>h", "<cmd>Rephrase<cr>" } })
+    end,
+})
