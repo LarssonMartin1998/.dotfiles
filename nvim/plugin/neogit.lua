240be2f734472450ed1b757ca06b7e38f4c32388
Martin Larsson <larssonmartin1998@gmail.com>
2026-04-26 01:30:22 +0200

    Fix norrsken highlights for neogit

--- nvim/plugin/neogit.lua
+++ nvim/plugin/neogit.lua
@@ -1,18 +1,25 @@
-vim.schedule(function()
-    vim.pack.add({
-        "https://github.com/nvim-lua/plenary.nvim",
-        "https://github.com/NeogitOrg/neogit",
-    })
-    local p = require("norrsken.palette")
-    require("neogit").setup({
-        highlight = {
-            italic = false,
-            bold = true,
-            underline = true,
-            green = p.green,
-            red = p.red,
-        },
-    })
-    require("norrsken.integrations.neogit")()
-    vim.keymap.set("n", "<leader>v", function() require("neogit").open() end)
-end)
+local utils = require("utils")
+
+utils.set_keymap_list({
+    { "<leader>v", function()
+        vim.pack.add({
+            "https://github.com/nvim-lua/plenary.nvim",
+            "https://github.com/NeogitOrg/neogit",
+        })
+        local p = require("norrsken.palette")
+        require("neogit").setup({
+            highlight = {
+                italic = false,
+                bold = true,
+                underline = true,
+                green = p.green,
+                red = p.red,
+            },
+        })
+        require("norrsken.integrations.neogit")()
+        utils.set_keymap_list({
+            { "<leader>v", function() require("neogit").open() end },
+        })
+        require("neogit").open()
+    end },
+})
